5f8d08485d39bb21fb289b85e6fc6823e6f12ed9
santoshreddyy <108527128+santoshreddyy@users.noreply.github.com>
2022-12-10 14:49:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-/*module "vpc" {
+module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "${var.project}-vpc"
@@ -8,9 +8,9 @@
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
 }
- */
+ 
  module "ecs-fargate-service" {
-  // depends_on                = [module.vpc]
+   depends_on                = [module.vpc]
    source                  = "./modules/ecs-fargate-service"
    vpc_id                  = var.vpc_id
    environment             = var.environment
