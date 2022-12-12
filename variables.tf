b1bf2fae9154a9c4382151bf7fb32b9586cec5da
santoshreddyy <108527128+santoshreddyy@users.noreply.github.com>
2022-12-12 12:40:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,5 +2,5 @@ variable "region" { default = "us-east-2" }
 variable "environment" { default = "test" }
 variable "project" { default = "javaweb" }
 variable "platform" { default = "Devops" }
-variable "vpc_id" { default = "vpc-0a8b4417f07580105" }
+variable "vpc_id" { default = " " }
 variable "team" { default = "devops4solutions" }
